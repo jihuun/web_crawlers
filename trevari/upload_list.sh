fbc2521319822bd6c0fa6497259b91ddad35158a
Ji-Hun Kim <jihuun.k@gmail.com>
2019-06-29 10:44:55 +0900

    trevari: tracing execute time whenever update
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- trevari/upload_list.sh
+++ trevari/upload_list.sh
@@ -6,38 +6,46 @@
 DIR=$(dirname "$0")
 SCRIPT="get_trevari_books.py"
 TARGET="trevari_book_list.md"
+
 function get_date {
 	echo $(date +%Y-%m-%d)
+}
+
+function get_time {
+	echo $(date +%T)
 }
 
 cd $DIR
 
 if [ -f $SCRIPT ]; then
-	echo "LOG: sync up with origin/master"
+	echo "[LOG]: sync up with origin/master"
 	git checkout master
 	git fetch origin
 	git rebase origin/master
-	echo "LOG: $SCRIPT is doing."
+	CUR_TIME=$(get_time)
+	echo "[LOG]: $SCRIPT is executed on $CUR_TIME"
 	python $SCRIPT 
-	echo "LOG: $SCRIPT done."
+	CUR_TIME=$(get_time)
+	echo "[LOG]: $SCRIPT is finished on $CUR_TIME."
 else 
-	echo "LOG: wrong path, curr dir is: $DIR"
+	echo "[LOG]: wrong path, curr dir is: $DIR"
 	exit
 fi
 
 DIFF=$(git diff $TARGET | grep "diff")
-echo "LOG: diff: $DIFF"
-echo "LOG: dir: $DIR pwd: $(pwd)"
+echo "[LOG]: diff: $DIFF"
+echo "[LOG]: dir: $DIR pwd: $(pwd)"
 
 if [[ -z "$DIFF" ]]
 then
-	echo "LOG: no diff"
+	echo "[LOG]: no diff"
 	exit
 else
 	cd $DIR
-	NOW=$(get_date)
-	echo "LOG: update commit - script: trevari: update book list on $NOW"
 	git add $TARGET
-	git commit -sm "script: trevari: update book list on $NOW (auto generated commit)"
+	git commit -sm "script: trevari: update book list on $TODAY (auto generated commit)"
 	git push origin master
+	TODAY=$(get_date)
+	CUR_TIME=$(get_time)
+	echo "[LOG]: update commit - script: trevari: update book list on $TODAY, $CUR_TIME"
 fi
